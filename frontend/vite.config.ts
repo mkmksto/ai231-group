8f0a19252a3bf2058732d9e8f48a3cc399295c7c
mkmksto <119323664+mkmksto@users.noreply.github.com>
2025-04-30 19:58:15 +0800

    feat(fe): configure vite proxy to be

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
+});
